3e00e8b834265c0b56cb650530f47cd8ecd019aa
Markus Tacker <m@coderbyheart.com>
2019-06-09 14:48:14 +0200

    feat: add script to set up webhooks

---
+++ src/set-up-webhooks.ts
@@ -0,0 +1,49 @@
+import { trelloApi } from './trello/api'
+import { config } from 'dotenv'
+
+config()
+
+const apiKey = process.env.API_KEY!
+const apiToken = process.env.API_TOKEN!
+const websiteContentList = process.env.WEBSITE_CONTENT_LIST!
+const netlifyWebhook = process.env.NETLIFY_WEBHOOK!
+
+const api = trelloApi({ apiKey, apiToken })
+
+Promise.all([
+  // Delete existing webhooks
+  api.tokens
+    .token({ token: apiToken })
+    .webhooks()
+    .then(webhooks =>
+      Promise.all(webhooks.map(({ id }) => api.webhook({ id }).delete())),
+    ),
+  // Fetch cards
+  api.lists.cards({
+    list: websiteContentList,
+  }),
+])
+  .then(([_, cards]) =>
+    Promise.all(
+      cards.map(({ id, name }) =>
+        api.webhooks.create({
+          active: true,
+          callbackURL: netlifyWebhook,
+          description: `Notify Netlify if ${name} card has changed`,
+          idModel: id,
+        }),
+      ),
+    ),
+  )
+  .then(() =>
+    api.tokens
+      .token({ token: apiToken })
+      .webhooks()
+      .then(webhooks => {
+        console.log(webhooks)
+      }),
+  )
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
